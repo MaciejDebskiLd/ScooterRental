683608f04faf942d175daa2fc9dd8fc84e4a0ebe
MaciejDebskiLD <maciej.debski@yahoo.com>
2019-11-24 11:02:50 +0100

    add dock controller test create user account controller test class

--- src/test/java/com/example/scooterrental/controller/ScooterDockControllerTest.java
+++ src/test/java/com/example/scooterrental/controller/ScooterDockControllerTest.java
@@ -1,4 +1,67 @@
 package com.example.scooterrental.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ScooterDockControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void ifGetScootersRequestIsCorrectShouldReturnHttpCode200AndInitialScooterList() throws Exception {
+        mockMvc.perform(get("/scooter-dock/{scooterDockId}/scooters", 1))
+                .andExpect(status().is(200))
+                .andExpect(content().json(
+                        "[{\n" +
+                        "\t\"id\": 6,\n" +
+                        "\t\"modelName\": \"RTT-43z\",\n" +
+                        "\t\"maxSpeed\": 20,\n" +
+                        "\t\"rentalPrice\": 15.50\n" +
+                        "},\n" +
+                        "{\n" +
+                        "\t\"id\": 7,\n" +
+                        "\t\"modelName\": \"V-SPEED-1\",\n" +
+                        "\t\"maxSpeed\": 35,\n" +
+                        "\t\"rentalPrice\": 29.99\n" +
+                        "},\n" +
+                        "{\n" +
+                        "\t\"id\": 8,\n" +
+                        "\t\"modelName\": \"V-SPEED-2\",\n" +
+                        "\t\"maxSpeed\": 40,\n" +
+                        "\t\"rentalPrice\": 39.99\n" +
+                        "},\n" +
+                        "{\n" +
+                        "\t\"id\": 5,\n" +
+                        "\t\"modelName\": \"ERE-321\",\n" +
+                        "\t\"maxSpeed\": 25,\n" +
+                        "\t\"rentalPrice\": 19.99\n" +
+                        "}]")
+                );
+    }
+
+    @ Test
+    public void ifGetScootersRequestContainsDockIdWhichNotExistShoulReturnHttpCode409AndError() throws Exception{
+        mockMvc
+                .perform(get("/scooter-dock/{scooterDockId}/scooters", 9999))
+                .andExpect(status().is(409))
+                .andExpect(content().json("{\n" +
+                        "\t\"errorCode\": \"Err008\",\n" +
+                        "\t\"errorMsg\": \"Dok o podanym id nie istnieje.\",\n" +
+                        "\t\"status\": \"ERROR\"\n" +
+                        "}"));
+    }
+
 }
