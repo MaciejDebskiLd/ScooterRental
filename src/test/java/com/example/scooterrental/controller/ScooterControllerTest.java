a288e756c2a6aa8ecde31568ddd5caa0e19c7741
MaciejDebskiLD <maciej.debski@yahoo.com>
2019-11-24 16:36:45 +0100

    migrate to sprig boot 2.2.1 UTF8 problem

--- src/test/java/com/example/scooterrental/controller/ScooterControllerTest.java
+++ src/test/java/com/example/scooterrental/controller/ScooterControllerTest.java
@@ -7,25 +7,29 @@ import com.example.scooterrental.model.UserAccount;
 import com.example.scooterrental.repository.ScooterRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import javax.validation.constraints.AssertTrue;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ScooterControllerTest {
@@ -61,7 +65,7 @@ public class ScooterControllerTest {
                 AddScooterResponse.class
         );
         Optional<Scooter> optionalScooter = scooterRepository.findById(response.getScooterId());
-        Assert.assertTrue(optionalScooter.isPresent());
+       assertTrue(optionalScooter.isPresent());
     }
 
 
--- src/test/java/com/example/scooterrental/controller/UserAccountControllerTest.java
+++ src/test/java/com/example/scooterrental/controller/UserAccountControllerTest.java
@@ -5,13 +5,13 @@ import com.example.scooterrental.model.UserAccount;
 import com.example.scooterrental.repository.UserAccountRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,11 +20,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserAccountControllerTest {
@@ -112,7 +113,7 @@ public class UserAccountControllerTest {
                 CreateUserAccountResponse.class
         );
         Optional<UserAccount> optionalUserAccount = userAccountRepository.findById(response.getAccountId());
-        Assert.assertTrue(optionalUserAccount.isPresent());
+        assertTrue(optionalUserAccount.isPresent());
     }
 
     @Test
